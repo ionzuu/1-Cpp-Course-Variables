b78bd02166121058137ae540fe2ffb81e729d712
Jonathan Costilla <99742932+ionzuu@users.noreply.github.com>
2023-10-13 00:52:23 -0500

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+    Variables
+    Initialization
+    Address
+    Alocate
+*/
+
+main(){
+    int var = 25;
+    /*
+        var = 25
+        & (Ampersand)
+        &nameofvariable
+    */
+   cout<<var<<endl;
+   cout<<&var<<endl;
+   var = 50;
+   cout<<var<<endl;
+   cout<<&var<<endl;
+   
+}
